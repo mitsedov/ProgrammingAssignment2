00c614742b6260e5c0b23739756f9f2506bab251
mitsedov <mitsedov@gmail.com>
2014-05-25 21:42:04 +0400

    Minor changes, final submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## The two functions below can be used to create an object that stores the value of a matrix  
-## and caches the value of the inverse of this matrix.
+## The two functions below can be used to create an object that stores the   
+## value of a matrix and caches the value of the inverse of this matrix.
 
-## The function makeCacheMatrix creates a list containing functions to 
+## The function makeCacheMatrix creates a matrix like object that can 
+## store its inverse. makeCacheMatrix function contains a list with 
+## functions to 
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
 ## 3. set the value of the inverse of the matrix
@@ -28,17 +30,23 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The function below solves for the inverse of the matrix-like object created by the function
-## makeCacheMatrix. The cacheSolve function checks if the inverse has already been calculated.
-## If yes, the inverse matrix is got from cache. Otherwise, the inverse matrix is calculated
-## and sets the value of the inverse in the cache with setinverse function.
+## The function below solves for the inverse of the matrix-like object 
+## created by the function makeCacheMatrix. The cacheSolve function 
+## checks if the inverse has already been calculated. If yes, the 
+## inverse matrix is taken from cache. Otherwise, the inverse matrix 
+## is calculated and sets the value of the inverse in the cache with 
+## setinverse function.
 
 cacheSolve <- function(x, ...) {
     inv <- x$getinverse()
 	if (!is.null(inv)){
-		message("getting cached data")
+		## This piece of code returns the inverse matrix 
+		## if it is already in cache
+		message("Getting cached inverse matrix")
 		return(inv)
 	}
+	## This piece of code computes the inverse and stores 
+	## it in cache if it wasn't done before 
 	mat <- x$get()
 	inv <- solve(mat)
 	x$setinverse(inv)
